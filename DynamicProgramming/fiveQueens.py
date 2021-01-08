3b06f689e6235b55d4c608c81810fad5036d8c44
Ryan Cummings <rcummings98@gmail.com>
2021-01-07 20:09:41 -0700

    initial commit

---
+++ DynamicProgramming/fiveQueens.py
@@ -0,0 +1,36 @@
+#-----------------------------------------
+# Simple Program using dynamic programming 
+# to solve the 5 queens problem
+#-----------------------------------------
+
+# ----------------------------------------
+# uses recursion with lookup table
+# to solve problem for given board
+# param: board : 2d array with location
+# of 0 to 5 queens
+# ----------------------------------------
+def solve(board, queens, i, j):
+    # base case: if all 5 queens are placed, return
+    if queens == 5:
+        return board
+    else:
+        while (i < 8):
+            while(j < 8):
+                board[i][j] = 1
+                check(board)
+        return solve(board);
+
+# -----------------------------------------
+# Check board for correct queen placement
+# -----------------------------------------
+def check(board):
+    for (i in range(8)):
+        if (board)
+
+
+# -----------------------------------------
+def main ():
+    
+    chessboard = [][]
+
+# -----------------------------------------
