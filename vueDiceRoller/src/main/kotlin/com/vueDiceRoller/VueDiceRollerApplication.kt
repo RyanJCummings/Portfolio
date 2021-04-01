0fbcc4c318a80fa82f80042aef533500460149f0
Ryan <rcummings98@gmail.com>
2021-04-01 11:40:09 -0500

    added vue front-end

---
+++ vueDiceRoller/src/main/kotlin/com/vueDiceRoller/VueDiceRollerApplication.kt
@@ -0,0 +1,11 @@
+package com.vueDiceRoller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VueDiceRollerApplication
+
+fun main(args: Array<String>) {
+	runApplication<VueDiceRollerApplication>(*args)
+}
---
+++ vueDiceRoller/src/test/kotlin/com/vueDiceRoller/VueDiceRollerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vueDiceRoller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VueDiceRollerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
