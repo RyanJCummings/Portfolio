ee00c4405a236815c48d85cccc99f885b13f3409
Ryan <rcummings98@gmail.com>
2021-04-10 11:19:34 -0500

    vue configuration

---
+++ vueDiceRoller/frontend/src/components/DiceRoller.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="dice">
+    <h1>Just Testing</h1>
+    <input v-model="dice" placeholder"which die?">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'dice',
+  }
+  </script>
